4da1e0c620270dc5974ca1c9542f74f0e6a1bdf0
Beacontownfc <1963697267@qq.com>
2022-05-15 20:35:50 +0800

    fix: del log

--- src/casdoor-uniapp-sdk.js
+++ src/casdoor-uniapp-sdk.js
@@ -20,7 +20,6 @@ export default {
     if (app.version.charAt(0) === '2') {
       app.prototype.getSigninUrl = () => {
         return CasdoorSDK.getSigninUrl();
-        console.log('test')
       }
 
       app.prototype.getSignupUrl = (enablePassword) => {
